a97c3ed87468cd80a5e9ce4d95c279e0f4e8e4f5
lynaDev2 <paulinatimothy3@gmail.com>
2026-04-27 13:38:33 +0100

    fix

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,3 +5,10 @@ export default {
     name: env('APP_NAME'),
     front_url: env('APP_FRONT_URL')
 }
+// import { env } from "src/utils/helpers";
+
+// export default {
+//     url: env('APP_URL'),
+//     name: env('APP_NAME'),
+//     front_url: env('APP_FRONT_URL')
+// }
--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -4,3 +4,10 @@ export default {
     expires_in: env('JWT_EXPIRES_IN'),
     secret: env('JWT_SECRET')
 }
+// import { env } from "src/utils/helpers";
+
+// export default {
+//     url: env('APP_URL'),
+//     name: env('APP_NAME'),
+//     front_url: env('APP_FRONT_URL')
+// }
